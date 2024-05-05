7890fa3a2212c71f81571f6c083b3b01e3b05af5
Petr Krysl <pkrysl@ucsd.edu>
2024-05-05 15:59:24 -0700

    fix not

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-if (!Base.Sys.islinux())
+if (Base.Sys.islinux())
     # -fPIC -shared for a shared library
     run(`g++ -O3 -march=native -fPIC -shared -fopenmp ECL-GC_12-lib.cpp -o ecl-gc.so`)
 else
