5bfd079961b802321801fd5b2617720e6cb5888b
Petr Krysl <pkrysl@ucsd.edu>
2024-05-05 15:56:35 -0700

    report unsupported

--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,6 @@
-# -fPIC -shared for a shared library
-run(`g++ -O3 -march=native -fPIC -shared -fopenmp ECL-GC_12-lib.cpp -o ecl-gc.so`)
+if (!Base.Sys.islinux())
+    # -fPIC -shared for a shared library
+    run(`g++ -O3 -march=native -fPIC -shared -fopenmp ECL-GC_12-lib.cpp -o ecl-gc.so`)
+else
+    @warn "Architecture not supported (so far only Linux)."
+end
