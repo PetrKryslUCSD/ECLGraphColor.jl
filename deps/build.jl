012b037b1131a1d61d33afe83a9e656c26c54d35
Petr Krysl <pkrysl@ucsd.edu>
2024-05-09 18:24:19 -0700

    add build for apple

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,9 @@
 if (Base.Sys.islinux())
     # -fPIC -shared for a shared library
     run(`g++ -O3 -march=native -fPIC -shared -fopenmp ECL-GC_12-lib.cpp -o ecl-gc.so`)
+elseif (Base.Sys.isapple())
+    # -fPIC -shared for a shared library
+    run(`armclang++ -O3 -march=native -fPIC -shared -fopenmp ECL-GC_12-lib.cpp -o ecl-gc.so`)
 else
-    @warn "Architecture not supported (so far only Linux)."
+    @warn "Architecture not supported (so far only Linux and Apple (armclang++ + libomp))."
 end
