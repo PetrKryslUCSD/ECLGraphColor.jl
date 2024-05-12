958e2d86a0d2724fabc5cdd4dbe6bd3d9751b870
Petr Krysl <pkrysl@ucsd.edu>
2024-05-12 15:41:56 -0700

    switch to clang++

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ if (Base.Sys.islinux())
     run(`g++ -O3 -march=native -fPIC -shared -fopenmp ECL-GC_12-lib.cpp -o ecl-gc.so`)
 elseif (Base.Sys.isapple())
     # -fPIC -shared for a shared library
-    run(`armclang++ -O3 -march=native -fPIC -shared -fopenmp ECL-GC_12-lib.cpp -o ecl-gc.so`)
+    run(`clang++ -O3 -march=native -fPIC -shared -fopenmp ECL-GC_12-lib.cpp -o ecl-gc.so`)
 else
     @warn "Architecture not supported (so far only Linux and Apple (armclang++ + libomp))."
 end
